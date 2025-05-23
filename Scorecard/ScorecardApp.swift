81e7a9121b99834d693286b68200b6694d9c4e02
Damian <gwozdziu1017@o2.pl>
2025-05-23 20:48:17 +0200

    Initial Commit

---
+++ Scorecard/ScorecardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScorecardApp.swift
+//  Scorecard
+//
+//  Created by Damian Gwóźdź on 23/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScorecardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
